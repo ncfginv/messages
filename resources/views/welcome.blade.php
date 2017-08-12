9da9519a5a0108aa225b441d68623672618ae8ac
takayuki <taka4852@i.softbank.jp>
2017-08-12 06:17:11 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Messages</h1>
-            {!! link_to_route('signup.get','Sign up now',null,['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        <?php $user = Auh::user(); ?>
+        {{ $user->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Messages</h1>
+                {!! link_to_route('signup.get','Sign up now!',null,['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
